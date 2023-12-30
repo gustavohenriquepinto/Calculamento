47fcd32e40fe34d52ebd9a7da8f16f7b5f1d7131
Gustavo Pinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2023-12-30 10:34:24 -0300

    Continuação NúmerosRomanos

--- Calculamento/BasesNumericas/Program.cs
+++ Calculamento/BasesNumericas/Program.cs
@@ -11,110 +11,56 @@ namespace BasesNumericas
     internal class Program
     {
         
-        static byte base1, base2, operacao;
-        static string txt1, txt2, alfabeto = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        static byte base1, base2;
+        static string txt1, txt2, algarismos = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         static long num1, num2;
-        static char sair;
 
-        //static char[] alfabeto = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-        
         static void Main(string[] args)
         {
-            
-
-            Inicio:
-
             Console.Clear();
-            Console.WriteLine("Bem-vindo ao sistema de cáculos com bases numéricas.");
-
-        #region PrimeiroNumero
-            Console.Write("\nDefina a base do primeiro número: ");
-            if (!byte.TryParse(Console.ReadLine(), out base1))
-            {
-                Console.WriteLine("Por favor, utiliza apenas números.");
-                goto Reiniciar;
-            }
-            else if (base1 > 36 || base1 < 2)
-            {
-                Console.WriteLine("Por favor, utilize números inteiros entre 2 e 36.");
-                goto Reiniciar;
-            }
-
-            Console.Write("\nDefina o valor do primeiro número: ");
-            txt1 = Console.ReadLine().ToUpper().Trim();
-
-            if (ContemApenasValidos(base1, txt1))
-            {
-                num1 = ConverterBase10(base1, txt1);
-            }
-            else
-            {
-                Console.WriteLine("Você inseriu caracteres inválidos.");
-                goto Reiniciar;
-            }
-            #endregion
-
-            //Console.WriteLine(num1.ToString());
-
-            Console.WriteLine("\nDeseja realizar qual operação?");
-            Console.WriteLine("1 - Conversão de Base");
-            Console.WriteLine("2 - Soma");
-            Console.WriteLine("3 - Subtração");
-            Console.WriteLine("4 - Multiplicação");
-            Console.WriteLine("5 - Divisão");
-
-            if(byte.TryParse(Console.ReadLine(), out operacao))
-            {
-                switch (operacao)
-                {
-                    case 1:
-
-                        break;
-                    case 2:
-
-                        break;
-                }
-            }
-            else
-            {
-
-            }
+            Console.WriteLine("Bem-vindo ao sistema de cáculos com bases numéricas.\n");
             
+            Inicio:
             
-
-        Reiniciar:
-
-            Console.Write("\nVocê deseja sair (S/N)?");
-            sair = char.Parse(Console.ReadLine());
-            if(sair !='s' && sair != 'S')
+            Console.Write("Defina a base do número a converter: ");
+            if (!BaseValida(Console.ReadLine()))
             {
+                Console.WriteLine("\nA base deve ser um número entre 2 e 36.");
                 goto Inicio;
+            }
+
+            Console.Write("Defina o número a converter: ");
+            txt1 = Console.ReadLine();
+                    
+            Console.Write("Defina a base do resultado: ");
+            if (byte.TryParse(Console.ReadLine(), out base1))
+            {
             }
         }
 
-        static bool ContemApenasValidos(byte baseDoNumero, string textoDoNumero)
+        static bool BaseValida(string txtBase)
         {
-            for (int letraDoAlfabeto = 0; letraDoAlfabeto < alfabeto.Length; letraDoAlfabeto++)
+            if (!byte.TryParse(txtBase, out base1))
             {
-                if (textoDoNumero.Contains(alfabeto[letraDoAlfabeto]) && letraDoAlfabeto > baseDoNumero)
+                return false;
+            }
+            else if (base1 < 2 || base1 > 36)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        static bool NumeroValido(string txtNumero)
+        {
+            foreach (char algarismo in txtNumero)
+            {
+                if (algarismos.IndexOf(algarismo) > base1)
                 {
                     return false;
                 }
             }
-
             return true;
-        }
-
-        static long ConverterBase10(byte baseDoNumero, string textoDoNumero)
-        {
-            long valorDoNumero = 0;
-
-            for(int caractereDoNumero = 0; caractereDoNumero < textoDoNumero.Length; caractereDoNumero++)
-            {
-                valorDoNumero += alfabeto.IndexOf(textoDoNumero[caractereDoNumero]) * (long)Math.Pow(baseDoNumero, (textoDoNumero.Length - (1 + caractereDoNumero)));
-            }
-
-            return valorDoNumero;
         }
     }
 }
--- Calculamento/NumerosRomanos/Program.cs
+++ Calculamento/NumerosRomanos/Program.cs
@@ -1,71 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NumerosRomanos
 {
-    internal class Program
+    sealed class Program
     {
-        static string txtDoNumero, txtDeBusca, letrasProibidas = "ABEFGHJKLNOPQRSTUWYZ\\'!¹@²#³$£%¢¨¬&*()-_+=§ ,<>.:;?/°^~`´{[ª}]º|\"";
-        static ulong numero, numeroDeBusca;
-        static bool contemApenasValidos;
-
-        //static SortedList<ulong, string> conversorRomano = new SortedList<ulong, string>()
-        //{
-        //    {1, "I" }, {2, "II" }, {3, "III" }, {4, "IV"}, {5, "V"}, {6, "VI"}, {7, "VII"}, {8, "VIII"}, {9,"IX"},
-        //    {10, "X" }, {20, "XX" }, {30, "XXX" }, {40, "XL"}, {50, "L"}, {60, "LX"}, {70, "LXX"}, {80, "LXXX"}, {90,"XC"},
-        //    {100, "C" }, {200, "CC" }, {300, "CCC" }, {400, "CD"}, {500, "D"}, {600, "DC"}, {700, "DCC"}, {800, "DCCC"}, {900,"CM"},
-        //    {1000, "M" }
-        //};
-
-
         static void Main(string[] args)
         {
-            Console.Clear();
-            Console.WriteLine("Bem-vindo ao sistema de conversão de números romanos.");
+            int valorNumero;
+            string textoNumero, textoRetorno = "", textoApoio;
+            char tipoConversao = 'A';
+
+            Dictionary<int, string> algarismosRomanos = new Dictionary<int, string>()
+            {
+                {1, "I"}, {2, "II"}, {3, "III"}, {4, "IV"}, {5, "V"}, {6, "VI"}, {7, "VII"}, {8, "VIII"}, {9, "IX"},
+                {10, "X"}, {20, "XX"}, {30, "XXX"}, {40, "XL"}, {50, "L"}, {60, "LX"}, {70, "LXX"}, {80, "LXXX"}, {90, "XC"},
+                {100, "C"}, {200, "CC"}, {300, "CCC"}, {400, "CD"}, {500, "D"}, {600, "DC"}, {700, "DCC"}, {800, "DCCC"}, {900, "CM"},
+                {1000, "M"}, {2000, "MM"}, {3000, "MMM"}
+            };
 
             Inicio:
+            Console.WriteLine("Bem-vindo ao sistema de conversão de números romanos.");
+            Console.Write("Digite o número a converter: ");
+            textoNumero = Console.ReadLine();
 
-            Console.Write("\nDigite o número que deseja converter: ");
-
-            txtDoNumero = Console.ReadLine().ToUpper();
-
-            if (ulong.TryParse(txtDoNumero, out numero))
+            if (int.TryParse(textoNumero, out valorNumero))
             {
-                if(numero < 1 || numero > 1000)
-                {
-                    Console.WriteLine("Digite um número entre 1 e 1000.");
-                    goto Inicio;
-                }
-                else
-                {
-                    
-                }
+                tipoConversao = 'A';
             }
             else
             {
-                contemApenasValidos = true;
-
-                foreach (char letra in letrasProibidas)
-                {
-                    if (txtDoNumero.Contains(letra))
-                    {
-                        contemApenasValidos = false;
-                    }
-                }
-
-                if (!contemApenasValidos)
-                {
-                    Console.WriteLine("Digite algarismos romanos ou indo-arábicos válidos.");
-                    goto Inicio;
-                }
-                else
-                {
-                    
-                }
+                Console.WriteLine("\nPor enquanto, apenas de algarismos indoarábicos para romanos.\n");
+                goto Inicio;
             }
+
+            switch (tipoConversao)
+            {
+                case 'A':
+
+                    if(valorNumero >= 4000 || valorNumero <= 0)
+                    {
+                        Console.WriteLine("\nEscolha valores maiores que 0 e menores que 4000.\n");
+                        goto Inicio;
+                    }
+
+                    for (int letra = 0; letra < textoNumero.Length; letra++)
+                    {
+                        if(algarismosRomanos.TryGetValue(textoNumero[letra] * (textoNumero.Length - 1 - letra), out textoApoio)){
+                            textoRetorno += textoApoio;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nOcorreu um erro.\n");
+                            goto Inicio;
+                        }
+                    }
+
+                    Console.WriteLine(textoRetorno);
+                    goto Inicio;
+
+                    break;
+
+                case 'B':
+                    break;
+                case 'C':
+                    break;
+            }
+
+
         }
     }
 }
